a64185c4f6f2e564fa419bf67be393fd84014452
Anna <annaoh1994@gmail.com>
2018-07-03 15:58:38 -0700

    Create Header component

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './stylesheets/Header.css';
+
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="header">
+        Anna Oh
+      </div>
+    );
+  }
+}
+
+export default Header;
